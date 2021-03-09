f3cfa39c626e490d017c545d7fd21b972ab56261
misaka-mikoto-39 <52187071+misaka-mikoto-39@users.noreply.github.com>
2021-03-09 15:57:48 +0700

    * Switch bot's behavior from code to Behavior tree

--- Source/SimpleShooter/AIControllers/ShooterAIController.cpp
+++ Source/SimpleShooter/AIControllers/ShooterAIController.cpp
@@ -21,18 +21,18 @@ void AShooterAIController::BeginPlay()
 void AShooterAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	if (LineOfSightTo(PlayerPawn))
-	{
-		/*SetFocus(PlayerPawn);
-		MoveToActor(PlayerPawn, AcceptanceRadius);*/
-		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
-		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
-	}
-	else
-	{
-		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
-		/*ClearFocus(EAIFocusPriority::Gameplay);
-		StopMovement();*/
-	}
+	//APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	//if (LineOfSightTo(PlayerPawn))
+	//{
+	//	/*SetFocus(PlayerPawn);
+	//	MoveToActor(PlayerPawn, AcceptanceRadius);*/
+	//	GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+	//	GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
+	//}
+	//else
+	//{
+	//	GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+	//	/*ClearFocus(EAIFocusPriority::Gameplay);
+	//	StopMovement();*/
+	//}
 }
